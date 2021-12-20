c13e52ecdce7c13c379887a5665d3ca74fd7b66c
Virtus Tan <virtustanofficial@gmail.com>
2021-12-20 23:29:14 +0700

    Master

--- scripting.js
+++ scripting.js
@@ -1,9 +1,9 @@
 
-var myVar;
+var myVar,class3=0;
 
 
 function myFunction() {
-    myVar = setTimeout(showPage, 300);
+    myVar = setTimeout(showPage, 2000);
   }
 
 function showPage() {
@@ -14,15 +14,17 @@ function showPage() {
 
 
 function gantiBahasa(){
-  let class1,class2,class3;
+  let class1,class2;
   class1 = document.getElementsByClassName("card-title");
   class2= document.getElementsByClassName("card-text");
-  if(class1[0].innerHTML === "About Me"){
+  if(class3 == 1){
     class1[0].innerHTML = "Tentang Saya"
     class2[0].innerHTML = "Halo, nama saya Virtus Tan. Tinggal di Yogyakarta, Indonesia. Menyukai komputer sejak masih TK. Saat ini melanjutkan studi ke dalam ilmu komputer khususnya dalam bidang pengembangan web."
+    class3 = 0;
   }else{
     class1[0].innerHTML = "About Me"
     class2[0].innerHTML = "Hi! My name is Virtus Tan. I live in Yogyakarta, Indonesia. I like everything about computers since I was in kindergarten. I'm currently pursuing my studies and careers in informatics engineering, especially for web development."
+    class3 = 1;
   }
 }
 
