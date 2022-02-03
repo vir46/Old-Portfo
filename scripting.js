e9181366ffcbb04f691131e22dec2e82357bb861
Virtus Tan <virtustanofficial@gmail.com>
2022-02-03 09:44:57 +0700

    Master

--- scripting.js
+++ scripting.js
@@ -3,14 +3,15 @@ var myVar,class3=0;
 
 
 function myFunction() {
-    myVar = setTimeout(showPage, 500);
+    // myVar = setTimeout(showPage, 500);
+    showPage(); 
   }
 
 function showPage() {
-    document.getElementById("loader").style.display = "none";
+    document.getElementById("loader").style.display = "none"; 
     document.getElementById("myDiv").style.animation = "fadeInAnimation ease 3s";
     document.getElementById("myDiv").style.opacity = 1;
-    $('#undernav').load('home.html');
+    $('#undernav').load('skill.html');
 }
 
 
@@ -23,25 +24,26 @@ function gantiBahasa(){
     class2[0].innerHTML = "Halo, nama saya Virtus Tan. Tinggal di Yogyakarta, Indonesia. Menyukai komputer sejak masih TK. Saat ini melanjutkan studi ke dalam ilmu komputer khususnya dalam bidang pengembangan web."
     class3 = 0;
   }else{
-    class1[0].innerHTML = "About Me"
+    class1[0].innerHTML = "About Me" 
     class2[0].innerHTML = "Hi! My name is Virtus Tan. I live in Yogyakarta, Indonesia. I like everything about computers since I was in kindergarten. I'm currently pursuing my studies and careers in informatics engineering, especially for web development."
     class3 = 1;
   }
 }
 
-
 function gotoPage(elementidpass){
     let csschange;
     csschange = document.getElementsByClassName("nav-link");
     for(let i=0;i<csschange.length;i++){
-      csschange[i].style.color = "grey";
-      csschange[i].style.fontWeight = "normal";
+      csschange[i].style.color = "grey"; 
+      csschange[i].style.fontWeight = "normal"; 
     }
     document.getElementById(elementidpass).style.color = "#fddb3a";
     document.getElementById(elementidpass).style.fontWeight = "bold";
     if(elementidpass == 'skillbutton'){
       $('#undernav').load('skill.html');
     }else if(elementidpass == 'homebutton'){
-      $('#undernav').load('home.html');
+      $('#undernav').load('home.html'); 
+    }else if(elementidpass == 'skillbutton'){
+      $('#undernav').load('gallery.html');
     }
 }
